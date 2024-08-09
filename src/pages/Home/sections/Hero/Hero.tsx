914521e3fa51cde5d89ff74ebd765577b36a5bd5
JaquelineMoraisFonseca <jaquelinemoraisprofissional@gmail.com>
2024-08-09 12:36:04 -0300

    Atualização de currículo

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -59,7 +59,7 @@ const Hero = () => {
                         <Typography color="primary.contrastText" variant="h1" textAlign="center">Jaqueline Morais</Typography>
                         <Grid container display="flex" justifyContent="center" spacing={3} pt={3} padding="50px">
                             <Grid item xs={12} md={6} display="flex" justifyContent="center">
-                                <StyledButton onClick={() => window.open("/my_portfolio/curriculo.pdf", "_blank")}>
+                                <StyledButton onClick={() => window.open("/my_portfolio/JaquelineMoraisFonseca_curriculo.pdf", "_blank")}>
                                     <DownloadIcon />
                                     <Typography  padding="7px">
                                         Currículo
